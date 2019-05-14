7d29c03c0f33355e9f389c6db0c6e8e0212db1e0
AlexMGalvez <Alexmarkgalvez@hotmail.com>
2019-05-14 01:25:21 -0400

    increase connection limt and timeout

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ let fs               = require("fs"),                 //file reader
 
 //mysql database connection
 var pool  = mysql.createPool({
-    connectionLimit     : 10,
+    connectionLimit     : 1000,
+    connectTimeout      : 60 * 60 * 1000,
+    acquireTimeout      : 60 * 60 * 1000,
+    timeout             : 60 * 60 * 1000,
     host                : process.env.DB_HOST,
     user                : process.env.DB_USER,
     password            : process.env.DB_PASS,
